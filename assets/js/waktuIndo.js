aa77966f7fd6080b31d71a236c183b315dd832a1
Vincentkled <129493544+Vincentkled@users.noreply.github.com>
2024-06-10 10:06:41 +0700

    Add files via upload

---
+++ assets/js/waktuIndo.js
@@ -0,0 +1,61 @@
+const dayID = () => {
+  if (day == 1) {
+    return 'Monday'
+  }
+  if (day == 2) {
+    return 'Tuesday'
+  }
+  if (day == 3) {
+    return 'Wednesday'
+  }
+  if (day == 4) {
+    return 'Thursday'
+  }
+  if (day == 5) {
+    return 'Friday'
+  }
+  if (day == 6) {
+    return 'Saturday'
+  }
+  if (day == 7) {
+    return 'Sunday'
+  }
+}
+const monthID = () => {
+  if (month == 0) {
+    return 'January'
+  }
+  if (month == 1) {
+    return 'February'
+  }
+  if (month == 2) {
+    return 'March'
+  }
+  if (month == 3) {
+    return 'April'
+  }
+  if (month == 4) {
+    return 'Mei'
+  }
+  if (month == 5) {
+    return 'June'
+  }
+  if (month == 6) {
+    return 'July'
+  }
+  if (month == 7) {
+    return 'August'
+  }
+  if (month == 8) {
+    return 'September'
+  }
+  if (month == 9) {
+    return 'October'
+  }
+  if (month == 10) {
+    return 'November'
+  }
+  if (month == 11) {
+    return 'December'
+  }
+}
